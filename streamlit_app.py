8fc5a0a77fae1eab4448bd7c371c663f7059d4e4
eulhch <129867999+eulhch@users.noreply.github.com>
2023-05-12 16:57:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
 
+import streamlit as st
+import pandas as pd
+
+conversation = {
+    "Hi": "Hello! How can I assist you today?",
+    "How are you?": "I'm doing well, thank you. How about you?",
+    "I'm good, thanks.": "Great to hear! How can I help you today?",
+    "Can you tell me a joke?": "Sure, why did the tomato turn red? Because it saw the salad dressing!",
+    "Goodbye": "Goodbye! Have a great day.",
+}
+
+with st.beta_container():
+    st.title("Dummy Robot")
+    user_input = st.text_input("User Input")
+    if st.button("Send"):
+        if user_input in conversation:
+            st.write(f"Robot: {conversation[user_input]}")
+        else:
+            st.write("Robot: Sorry, I didn't understand what you said. Please try again.")
+```
